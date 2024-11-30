25b41fa65b47ee98ff9ac7bda296d43b8061f731
Melizandra Romero <melizandraromeroromero@gmail.com>
2024-11-30 14:40:43 -0600

    Actualización nueva

--- src/Trabajadores/Docente.js
+++ src/Trabajadores/Docente.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import GraficoClientes from './GraficoClientes';
-import GraficoAlumnos from './GraficoAlumnos'; // Importa el nuevo componente
+import GraficoAlumnos from './GraficoAlumnos';
+
 
 const ListaClientes = () => {
     const [clientes, setClientes] = useState([]);
